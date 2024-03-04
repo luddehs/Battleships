9751fb07864e0b784fe6d714cea94d3b1be2cee8
Ludvig Hultgren Svensson <ludvig.hs@gmail.com>
2024-03-04 12:46:18 +0100

    Change class instances in new_game function and a population for loop.

--- run.py
+++ run.py
@@ -9,16 +9,14 @@ class Board:
 
     def __init__(self, size, num_ships, name, type):
         """
-        Initialize the board game object with the given size and game attributes
+        Initialize the board game object with the given size and game attributes.
         """
         self.size = size
         self.grid = [['0' for x in range(size) for y in range(size)]]
         self.num_ships = num_ships
-        self.name = name
         self.type = type
-        self.guesses = []
         self.ships = {}
-
+    
 
 def random_point(size):
     """."""
@@ -32,6 +30,7 @@ def populate_board():
     Returns a tuple containing computer and player board.
 
     """
+    global size
     size = 10 # Board size can be changed here.
     computer_board = Board(size)
     player_board = Board(size)
@@ -48,8 +47,9 @@ def new_game():
     """
     Starts a new game, configuring the board size and ship count, clearing scores, and initializing the game boards.
     """
+    size = 5
+    num_ships = 5
     scores = {"computer": 0, "player": 0}
-    
     print("""
 ▀█████████▄     ▄████████     ███         ███      ▄█          ▄████████    ▄████████    ▄█    █▄     ▄█     ▄███████▄    ▄████████
   ███    ███   ███    ███ ▀█████████▄ ▀█████████▄ ███         ███    ███   ███    ███   ███    ███   ███    ███    ███   ███    ███
@@ -64,8 +64,13 @@ def new_game():
     print("Let's start a new game!\n")
     player_name = input("Please enter your name: \n")
     
-    computer_board, player_board = populate_board()
-    play_game(computer_board, player_board)
+    computer_board = Board(size, num_ships, "Computer", type="computer")
+    player_board = Board(size, num_ships, player_name, type="player")
 
+    for _ in range(num_ships):
+        populate_board(player_board)
+        populate_board(computer_board)
+
+    play_game(computer_board, player_board)
 
 new_game()
